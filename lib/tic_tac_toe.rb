bbf56184249f1d2100c722c594187c9612897578
juddB <Juddbahoui@gmail.com>
2021-02-07 22:15:56 -0500

    commit tree 6811c8da519baf9a09e8dacb2f8d0bba82b9e982

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,134 @@
+class TicTacToe
+    attr_accessor :board
+  
+    def initialize(board = nil)
+        @board = board || Array.new(9, " ")
+    end
+  
+    WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [6, 4, 2],
+    [0, 4, 8]
+  ]
+  
+    def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts " ----------- "
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts " ----------- "
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+  
+    def input_to_index(input)
+      input.to_i - 1
+    end
+  
+    def move(position, token='X')
+      @board[position] = token
+    end
+  
+    def position_taken?(input)
+      @board[input] == "X" || @board[input] == "O"
+    end
+  
+    def valid_move?(index)
+      index.between?(0,8) && !position_taken?(index)
+    end
+  
+    def turn_count
+      taken = 0
+      @board.each do |i|
+        if i == "X" || i == "O"
+          taken += 1
+        end
+      end
+      return taken
+    end
+  
+    def current_player
+      player = nil
+      if turn_count % 2 == 0
+        player = 'X'
+      else
+        player = 'O'
+      end
+      return player
+    end
+
+    def turn
+      puts "Pick a number from 1 to 9"
+      user_input = gets.strip
+      user_index = input_to_index(user_input)
+      if valid_move?(user_index) 
+          move(user_index, current_player)
+      else 
+          turn 
+      end
+      display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return win_combo
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return win_combo
+      end
+        false
+    end
+  end
+  
+  #full?
+  def full?
+    @board.all?{|occupied| occupied != " "}
+  end
+  
+  #draw
+  def draw?
+    !(won?) && (full?)
+  end
+  
+  #over?
+  def over?
+    won? || full? || draw?
+  end
+  
+  #winner?
+  def winner
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return "X"
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return "O"
+      else
+        nil
+      end
+    end
+  end
+
+
+  def play
+    until over?
+    turn
+    end
+    if won?
+    puts "Congratulations " + winner + "!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
+
+   
+  
+require_relative '../lib/tic_tac_toe.rb'
+
+game = TicTacToe.new
+
+ 
